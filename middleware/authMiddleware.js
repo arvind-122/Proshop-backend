cc0a414a62c2eb13c8bad67c7e361e4960c7afa2
unknown <rawatarvind718@gmail.com>
2023-01-30 11:45:10 +0530

    bug fixed

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import User from "../models/UserModel.js";
+import User from "../Models/UserModel.js";
 import expressAsyncHandler from "express-async-handler";
 
 dotenv.config();
